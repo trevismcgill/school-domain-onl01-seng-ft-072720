d07f2968f7244931f81034d4b13b946b4c147977
Trevis McGill <trevismcgill25@gmail.com>
2020-08-04 21:06:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require "pry"
+class School
+attr_accessor :roster, :name, :grade
+def initialize(arg)
+@roster = {}
+end
+
+
+def add_student(name, grade)
+if @roster[grade]
+  @roster[grade].push(name)
+else
+  @roster[grade] = []
+  @roster[grade].push(name)
+end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |key, value|
+    value.sort!
+end
+end
+
+end
